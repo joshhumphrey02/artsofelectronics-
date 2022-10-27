0eec057a8960093fef9262ad5ae94784d8fd2de0
joshhumphrey02 <85317065+joshhumphrey02@users.noreply.github.com>
2022-10-27 23:10:09 +0100

    bugs fixed

--- public/js/mobile/side_menu.js
+++ public/js/mobile/side_menu.js
@@ -1,8 +1,22 @@
 const div = (div)=>document.querySelector(div);
 
-div('#menu').addEventListener('touchstart', ()=>{
-    div('.side_menu').style.display = 'block';
+div('#menu').addEventListener('click', ()=>{
+    div('.side_menu').showModal();
 })
-div('.side_menu').addEventListener('touchstart', ()=>{
-    div('.side_menu').style.display = 'none';
+div('body').addEventListener('click', (e)=>{
+    if(e.target.classList == "side_menu"){
+        div('.side_menu').close();
+    }
 })
+
+
+function Load (){
+    fetch('/getCart')
+      .then(res => res.json()) 
+      .then(data => {
+          if(data.length > 0){
+            div('.cart_length').innerHTML = data.length;
+          }
+      })
+}
+Load();
